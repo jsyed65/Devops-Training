46aefdb7ff7cedd004ff3f4d72b8a6934d4c7fa7
jsyed65 <138546841+jsyed65@users.noreply.github.com>
2023-11-03 00:15:38 +0530

    Add files via upload

---
+++ Colors.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+		
+		#Display all the colors
+		num=1
+		
+		for colors in Orange Red Yellow Green Blue  Purple White  Pink
+		do
+		echo "$num: $colors"
+		((num++))
+		done
---
+++ addition.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Enter the first number"
+read num1
+echo "Enter the second number"
+read num2
+
+
+
+				(( sum=num1+num2 ))
+				
+				echo "The result of addition: $sum"./   
+
+                echo "Enter the first name"
+                read first name
+                echo "Enter the second name"
+                read last name
