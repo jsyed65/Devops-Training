f796b408853e5360495317d8ba4a181bc1fc6764
unknown <javeedsyed061089@gmail.com>
2023-12-28 08:58:46 +0530

    update

---
+++ Assignment1/Multiplication.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Enter the first number"
+read num1
+echo "Enter the second number"
+read num2
+
+
+
+(( mul=num1*num2 ))
+
+ echo "The result of multiplication: $mul"
+				
+			
---
+++ Terraform/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+region = "us-east-1"
+access_key = var.access_key
+secret_key = var.secret_key
+}
+
+resource "aws_instance" "demo-instance" {
+ami                    = "ami-0759f51a90924c166"
+instance_type          = "t2.micro"
+			  
+tags = {
+Name  = "demo"
+Owner = "Terraform"
+ }
+}
